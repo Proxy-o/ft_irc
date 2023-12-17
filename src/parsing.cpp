b08c2f2736152251962937eafc4eefe10709e3ed
proxy-o <othmane@gmail.com>
2023-12-17 17:44:08 +0100

    init

--- src/parsing.cpp
+++ src/parsing.cpp
@@ -7,6 +7,7 @@ int Server::parseMessage(int fd)
     std::string message = client.getRecvBuffer();
     if (message.find("\n") != std::string::npos)
     {
+
         std::vector<std::string> lines = ft_split(message, "\n");
         std::vector<std::string>::iterator it = lines.begin();
         for (; it != lines.end(); it++)
