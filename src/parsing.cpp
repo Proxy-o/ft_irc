d5188cf34029e1795d362b6a7e5f1de42d6b4286
proxy <you@example.com>
2023-12-24 12:16:32 +0100

    enable auth

--- src/parsing.cpp
+++ src/parsing.cpp
@@ -24,8 +24,8 @@ int Server::parseMessage(int fd)
             formatMessage(line);
             if (client.isRegistered() == false && isValidCommand(line) == true)
                 registerClient(line, client);
-            // else if (client.isRegistered() && isValidCommand(line) == true)
-            // {
+            else if (client.isRegistered() && isValidCommand(line) == true)
+            {
                 if (line.find("NICK") == 0)
                 {
                     nick(line, client, *this);
@@ -50,11 +50,11 @@ int Server::parseMessage(int fd)
                 {
                     join(line, client, *this);
                 }
-            //  }
-                // else
-                // {
-                //     client.setSendBuffer("INVALID COMMAND: " + line +"\r\n");
-                // }
+             }
+                else
+                {
+                    client.setSendBuffer("INVALID COMMAND: " + line +"\r\n");
+                }
             client.resetRecvBuffer();
         }
     }
